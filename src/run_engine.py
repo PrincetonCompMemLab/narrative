f0a2a61fbd619b83c48f1f96bb202116274b899d
Momchil Tomov <tomov90@gmail.com>
2017-12-07 12:37:50 -0500

    Return scene vector s_1:n and event vector e_1:n in a format that's convenient for HRR encoding and passing to SEM.
    Apologies Q that I'm using the world "scene" instead of "state". We're
    trying to stick with that nomenclature as "state" is an ambiguous term
    that can stand for many other things in the model.
    
    Also fixed a couple of small bugs:
    - cond_fill is not always unicode and was erroring out with
    AttributeError: 'str' object has no attribute 'isnumeric'
    - when GEN_SYMBOLIC_STATES and ATTACH_ROLE_MARKER are both set, the
    role-filler pairs were printed multiple times (e.g. in the Dessert_crumble scene). I added
    the vector used_fillers to keep track of the fillers independent of the
    roles.

--- src/run_engine.py
+++ src/run_engine.py
@@ -20,7 +20,10 @@ n_input_files = len(input_fnames)
 names_concat = '_'.join(input_fnames)
 
 
-# sample stories from schema
+# sample stories from one or more schemas
+# Returns a scene (state) vector s_1:n formatted for HRR encoding
+# as well as a sequence of event labels e_1:n according to the story type
+#
 def main(rand_seed):
     # get a handle on the output file
     output_path = mkdir(names_concat, n_iterations, n_repeats)
@@ -32,15 +35,22 @@ def main(rand_seed):
         schema_info.append(read_schema_file(input_fnames[i]))
 
     # write stories with alternating schema info
+    scenes = [] # s_1:n
+    events = [] # e_1:n
     for i in range(n_input_files * n_iterations):
         f_idx = np.mod(i, n_input_files)
         # write to the output file
-        rand_seed = write_stories(schema_info[f_idx],
+        rand_seed, schema_scenes = write_stories(schema_info[f_idx],
                                   f_stories, f_QA,
                                   rand_seed, n_repeats)
+        scenes.extend(schema_scenes)
+        events.extend([f_idx] * len(schema_scenes))
 
+    assert len(events) == len(scenes)
     f_stories.close()
     f_QA.close()
+
+    return scenes, events
 
 if __name__ == "__main__":
     # if there is only one 1 schema file, repeat & iter are the same thing
